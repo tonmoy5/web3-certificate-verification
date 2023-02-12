067730733eebfcb7ce8401c3f3c72ca8c21c09cd
tonmoy5 <tonmoy52532@gmail.com>
2023-02-12 12:03:17 +0600

    Old address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,7 @@ import abi from './certificate.json'
 
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x0EB5905DAca445C3968B47583e1741DfCe7404C3'
-
-//'0xd59441a2730975DFf548BfFaA0830d9f5d82596f'
+export const contractAddress = '0xd59441a2730975DFf548BfFaA0830d9f5d82596f'
 
 export const admin = {
   "email": "admin@certauth.tech",
